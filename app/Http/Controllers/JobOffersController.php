68f968a985bc70fad4e655500f1ad88079cc29f8
Isaac Mosqueda <yosoy@mosquedacordova.com>
2024-05-27 16:44:37 +0000

    CRUD OFERTAS DE EMPLEO

--- app/Http/Controllers/JobOffersController.php
+++ app/Http/Controllers/JobOffersController.php
@@ -6,10 +6,31 @@ use Illuminate\Http\Request;
 use App\Models\JobOffers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
 
 class JobOffersController extends Controller
 {
 
+    /**
+     * Retorna todas las ofertas de empleo.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            // Obtiene todas las ofertas de empleo
+            $jobOffers = JobOffers::all();
+
+            // Retorna una respuesta con las ofertas de empleo
+            return response()->json(['data' => $jobOffers], 200);
+        } catch (\Exception $e) {
+            // Loguea el error
+            Log::error('Error al obtener las ofertas de empleo: ' . $e->getMessage());
+            return response()->json(['error' => 'Error al obtener las ofertas de empleo'], 500);
+        }
+    }
+    
     /**
      * Almacena una nueva oferta de trabajo.
      *
@@ -48,4 +69,50 @@ class JobOffersController extends Controller
             return response()->json(['error' => 'Error al guardar la oferta de trabajo'], 500);
         }
     }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|required|string',
+            'whatsapp' => 'nullable|string',
+            'mail' => 'nullable|string|email',
+            'status' => 'sometimes|required|integer', // Agregar validación para el estado
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        try {
+            $jobOffer = JobOffers::findOrFail($id);
+            $jobOffer->update($request->all());
+
+            return response()->json(['message' => 'Oferta de trabajo actualizada con éxito', 'data' => $jobOffer], 200);
+        } catch (\Exception $e) {
+            Log::error('Error al actualizar la oferta de trabajo: ' . $e->getMessage());
+            return response()->json(['error' => 'Error al actualizar la oferta de trabajo'], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $jobOffer = JobOffers::findOrFail($id);
+            $jobOffer->delete();
+
+            return response()->json(['message' => 'Oferta de trabajo eliminada con éxito'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('Error al eliminar la oferta de trabajo: ' . $e->getMessage());
+            return response()->json(['error' => 'Error al eliminar la oferta de trabajo'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
+
+
+
+
+
+
+
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobOffersController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::put('/job-offers/{id}', [JobOffersController::class, 'update']);
+    Route::delete('/job-offers/{id}', [JobOffersController::class, 'destroy']);
+
+});
+
+Route::get('/job-offers', [JobOffersController::class, 'index']);
 Route::post('/job-offers', [JobOffersController::class, 'store']);
-
-Route::get('/test', [JobOffersController::class, 'test']);
 
